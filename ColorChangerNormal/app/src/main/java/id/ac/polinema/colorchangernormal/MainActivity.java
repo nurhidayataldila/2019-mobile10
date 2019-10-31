87e38e32204c5e40799e9cd449f1b2783ab9b52e
Nurhidayat Aldila <nurhidayat.aldila@gmail.com>
2019-10-31 22:14:44 +0700

    Tambah library lifecycle, Menambahkan class ColorViewModel, Menambah atribut colorviewmodel, Instansiasi ViewModel, Ganti background menggunakan ViewModel, Update logic event dengan menambahkan colorviewmodel

--- ColorChangerNormal/app/src/main/java/id/ac/polinema/colorchangernormal/MainActivity.java
+++ ColorChangerNormal/app/src/main/java/id/ac/polinema/colorchangernormal/MainActivity.java
@@ -2,6 +2,7 @@ package id.ac.polinema.colorchangernormal;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.graphics.Color;
 import android.os.Bundle;
@@ -15,20 +16,25 @@ public class MainActivity extends AppCompatActivity {
 	private ConstraintLayout rootView;
 	private Button btnChangeColor;
 
+	ColorViewModel colorViewModel;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		rootView = findViewById(R.id.layout_main);
 		btnChangeColor = findViewById(R.id.change_color);
+
+		colorViewModel = ViewModelProviders.of(this).get(ColorViewModel.class);
 		
-		rootView.setBackgroundColor(generateRandomColor());
+		rootView.setBackgroundColor(colorViewModel.getColor());
 
 		// Tambahkan event klik pada tombol
 		btnChangeColor.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View view) {
 				int color = generateRandomColor();
+				colorViewModel.setColor(color);
 				rootView.setBackgroundColor(color);
 			}
 		});
